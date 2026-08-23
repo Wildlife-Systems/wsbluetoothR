861af4aaf02d1c01bd04f0cc00e9f0074828628a
Ed Baker <edwbaker@gmail.com>
2026-08-23 12:12:51 +0100

    Cleanup

--- test_duration_small.R
+++
@@ -1,8 +0,0 @@
-# Test duration analysis with small data file
-library(wsbluetoothR)
-
-# Test with 100 lines
-cat("Testing with 100 lines...\n")
-result <- get_address_duration("data/test_head100.txt", verbose = TRUE)
-print(head(result))
-cat("\nSuccess!\n")
